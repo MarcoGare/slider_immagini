6a420c371dcf6d91ae5c32db1d6debfc2b1735f2
Marco Garello <garellomarco@itis-molinari.eu>
2025-02-17 17:01:43 +0100

    modifica index

--- public/index.js
+++ public/index.js
@@ -1,48 +1,64 @@
+import { createNavigator } from "./navigator.js";
+import {login} from"./login.js";
+const navigator = createNavigator(document.querySelector("#container"));
+const log=login();
+
 const creazioneMiddleware = () => {
-    return {
-        load: async () => {
+    return{
+        load: async () =>{
             const response = await fetch("/images");
-            const json = await response.json();
-            return await json;
-        },
-        delete: async (id) => {
-            const response = await fetch(`/delete/` + id, 
-            { method: "DELETE" });
             const json = await response.json();
             return json;
         },
-        upload: async (inputFile) => {
+        delete: async (id) =>{
+            const response = await fetch("/delete/" + id, {
+                method: "DELETE",
+            });
+            const json = await response.json();
+            return json;
+        },
+        upload: async(inputFile) => {
             const formData = new FormData();
             formData.append("file", inputFile.files[0]);
             const body = formData;
-            const fetchOptions = { 
-                method: "POST",
-                body: body 
+            const fetchOptions = {
+                method: 'post',
+                body: body
             };
             try{
-                const res = await fetch("/upload", fetchOptions);
+                const res = await fetch ("/upload", fetchOptions);
                 const data = await res.json();
                 console.log(data);
-                
-            } catch (e){
-                console.log(e)
-            }
+            }catch (e){
+                console.log(e);
+            };
         }
     };
 };
 
-const controller = async (middleware) => {
-    const template = `
-    <tr>
-        <td><img src="$URL" style="width: 600px; height: 600px;" /></td>
-        <td> button id= "$ID" type= "button" class= "delete btn btn-button" style="background-color:blue">ELIMINA</button></td>
-        </tr>`;
-    
-    const render = (list) => {
-    };
-    
+const controller = async (middleware) => { 
     const inputFile = document.querySelector("#file");
-    const button = document.querySelector("#button");
+    const button = document.querySelector("#button"); 
+    const tables =document.getElementById("#tables")
+    const praga =document.getElementById("#praga")
+
+
+    const renderPraga =(list)=>{
+        let html = '';
+        for (let i=0; i < list.lenght; i++){
+            const foto = list[i];
+            let active = '';
+            if (i=== 0){
+                active= 'active';
+            }
+            html += '<div id="' + foto.id + '" class="carousel-item' + active + '">'+
+            '<img src="' + foto.name + '" style="width: 1000px; height: 600px;" alt="Immagine ' + foto.id + '" />'+
+            '</div>';
+        }
+        praga.innerHTML =html;
+    };
+
+    
     button.onclick = async () => {
         await middleware.upload(inputFile);
         middleware.load().then(render);
